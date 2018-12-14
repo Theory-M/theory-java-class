6a250ee5250ffc20f5e398d1b4e37b6ec8521713
theory <mic.juszczak@gmail.com>
2018-12-14 20:57:11 +0100

    [change] - classes 09.12.18^^^

---
+++ javaclass1/homework/beverages_stream_practice/Allergens.java
@@ -0,0 +1,5 @@
+package pl.learn.theory.javaclass1.homework.beverages_stream_practice;
+
+public class Allergens {
+
+}
---
+++ javaclass1/homework/beverages_stream_practice/Beverage.java
@@ -0,0 +1,10 @@
+package pl.learn.theory.javaclass1.homework.beverages_stream_practice;
+
+import java.util.List;
+
+public class Beverage {
+    private String name;
+    private List<Allergens> allergens;
+    private boolean promo;
+    private int price;
+}
